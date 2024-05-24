2e8119f0c7f7176ddb4a8df8f83e2108522dc026
JohnyKapo <janikkapral1234@gmail.com>
2024-05-24 20:02:44 +0200

    restructed project

--- CarHutChat/CarHutChatHistoryService/src/main/java/com/carhutchathistoryservice/repository/CreatedChatsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.carhutchathistoryservice.repository;
-
-import com.carhutchathistoryservice.models.CreatedChats;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CreatedChatsRepository extends JpaRepository<CreatedChats, String> {
-}
